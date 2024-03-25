31046207cb183b3acab8c9d8a9afb4b99ff8507e
umar <umerfaqrooq78@gmail.com>
2024-03-26 01:50:14 +0500

    deploy --version=3

--- rio_qrcode_app/settings.py
+++ rio_qrcode_app/settings.py
@@ -134,5 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "/app/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
